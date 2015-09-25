f916b2f3c56c900c0d20a9016d70837c98ced463
Charles Edwards <charles.edwards@niwa.co.nz>
2015-09-25 16:35:58 +1200

    updated help for BioOptions
    not sure this is the best way to describe it

--- R/BioOptions.R
+++ R/BioOptions.R
@@ -1,11 +1,22 @@
 #' Biomass plot options
 #'
-#' Parameters to be passed on to biomass plotting functions
+#' List of options to be passed to biomass plotting functions. This list is loaded with \pkg{RLPlots} and
+#' is called by the functions \code{\link{Bio}}, \code{\link{Bio_posterior}}, \code{\link{MPD.All}} 
+#' and \code{\link{MCMC.All}}. It contains a list of logical values that can be modified by the user 
+#' and passed to the plotting function. Elements in the list include the following logical values:
+#' \describe{
+#'   \item{\code{VulnB = TRUE}}{do the vulnerable biomass plot}
+#'   \item{\code{TotalB = TRUE}}{do the total biomass plot}
+#'   \item{\code{RecB = TRUE}}{do the recruited biomass plot}
+#'   \item{\code{TotalRecB = TRUE}}{do the x plot}
+#' }
 #' 
-#' @param VulnB logical: do the vulnerable biomass plot
-#' @param TotalB logical: do the total biomass plot
-#' @param RecB logical: do the recruited biomass plot
-#' @param TotalRecB logical: do the x plot
-#' @export
-#' 
-.BioOptions <- list(VulnB = TRUE, TotalB = TRUE, RecB = TRUE, TotalRecB = TRUE)
+#' @examples
+#' \dontrun{
+#' Bio("CRA5", source.dir = ".", BioOptions = list(VulnB = TRUE, 
+#'                                                  TotalB = TRUE, 
+#'                                                  RecB = TRUE, 
+#'                                                  TotalRecB = TRUE))
+#' }
+BioOptions <- list(VulnB = TRUE, TotalB = TRUE, RecB = TRUE, TotalRecB = TRUE)
+.BioOptions <- BioOptions
