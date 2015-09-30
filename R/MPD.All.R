5755971cf10c70bb3acbc889d9b2cbf450b5eb9a
D'Arcy Webber <darcy@quantifish.co.nz>
2015-09-30 13:24:38 +1300

    added try catch statements to MPD.All plotting software

--- R/MPD.All.R
+++ R/MPD.All.R
@@ -9,42 +9,45 @@ MPD.All <- function(stock, source.dir = ".", target.dir = paste('../', stock, 'p
                     LFOptions = .LFOptions, TagOptions = .TagOptions,
                     BioOptions = .BioOptions, SEXROptions = .SEXROptions)
 {
-    if (!dir.exists(target.dir)) {
+    if (!dir.exists(target.dir))
+    {
         dir.create(target.dir)
-		message('created target.dir ', target.dir)
+        message('created target.dir ', target.dir)
     }
-	
+    emsg <- "OOPS - EITHER YOU FUCKED UP OR THE CODE DID, the original error message is:"
+    efun <- function(e) { message(emsg); message(e) }
+
     message("Plotting catches")
-    Catches(stock, source.dir, target.dir, PlotOptions)
+    tryCatch(Catches(stock, source.dir, target.dir, PlotOptions), error = efun, finally = NULL)
     message("Plotting CPUE")
-    CPUE(stock, source.dir, target.dir, CPUEOptions, PlotOptions)
+    tryCatch(CPUE(stock, source.dir, target.dir, CPUEOptions, PlotOptions), error = efun, finally = NULL)
     message("Plotting catch rates")
-    CR(stock, source.dir, target.dir, CROptions, PlotOptions)
+    tryCatch(CR(stock, source.dir, target.dir, CROptions, PlotOptions), error = efun, finally = NULL)
     # vh: comment out PRI plots cause crashes when no PRI output files
     # POO(stock,source.dir,target.dir, CROptions, PlotOptions)
     message("Plotting length-frequencies")
-    LF(stock, source.dir, target.dir, LFOptions, PlotOptions)
+    tryCatch(LF(stock, source.dir, target.dir, LFOptions, PlotOptions), error = efun, finally = NULL)
     message("Plotting tag-recaptures")
-    Tag(stock, source.dir, target.dir, TagOptions, PlotOptions)
+    tryCatch(Tag(stock, source.dir, target.dir, TagOptions, PlotOptions), error = efun, finally = NULL)
     message("Plotting Erate")
-    Erate(stock, source.dir, target.dir, PlotOptions)
+    tryCatch(Erate(stock, source.dir, target.dir, PlotOptions), error = efun, finally = NULL)
     message("Plotting recruitment")
-    Rect(stock, source.dir, target.dir, PlotOptions)
+    tryCatch(Rect(stock, source.dir, target.dir, PlotOptions), error = efun, finally = NULL)
     message("Plotting LFzero")
-    LFzero(stock, source.dir, target.dir, PlotOptions)
+    tryCatch(LFzero(stock, source.dir, target.dir, PlotOptions), error = efun, finally = NULL)
     message("Plotting Maturity")
-    Mature(stock, source.dir, target.dir, PlotOptions)
+    tryCatch(Mature(stock, source.dir, target.dir, PlotOptions), error = efun, finally = NULL)
     message("Plotting Selectivity")
-    Select(stock, source.dir, target.dir, PlotOptions)
+    tryCatch(Select(stock, source.dir, target.dir, PlotOptions), error = efun, finally = NULL)
     message("Plotting Moult")
-    Moult(stock, source.dir, target.dir, PlotOptions)
+    tryCatch(Moult(stock, source.dir, target.dir, PlotOptions), error = efun, finally = NULL)
     message("Plotting mean size")
-    MeanSize(stock, source.dir, target.dir, LFOptions, PlotOptions)
+    tryCatch(MeanSize(stock, source.dir, target.dir, LFOptions, PlotOptions), error = efun, finally = NULL)
     message("Plotting biomass")
-    Bio(stock, source.dir, target.dir, BioOptions, PlotOptions)
+    tryCatch(Bio(stock, source.dir, target.dir, BioOptions, PlotOptions), error = efun, finally = NULL)
     message("Plotting SPBrect")
-    SPBrect(stock, source.dir, target.dir, PlotOptions)
+    tryCatch(SPBrect(stock, source.dir, target.dir, PlotOptions), error = efun, finally = NULL)
     #DDgrowth(stock,source.dir,target.dir, PlotOptions)
     message("Plotting sex ratio")
-    sexRatio(stock, source.dir, target.dir, SEXROptions, PlotOptions)
+    tryCatch(sexRatio(stock, source.dir, target.dir, SEXROptions, PlotOptions), error = efun, finally = NULL)
 }
--- R/sexRatio.R
+++ R/sexRatio.R
@@ -1,4 +1,4 @@
-#' sex ratio fit plots
+#' Sex ratio fit plots
 #'
 #' @param run the directory where the outputs for a run are sitting
 #' @param stock the name of the stock (e.g. CRA1)
@@ -11,134 +11,140 @@ sexRatio <- function(stock, source.dir, target.dir = source.dir,
 {
     filename <- paste(source.dir, "/", stock, "SexRatio.out", sep = "")
     
-    if (file.exists(filename)) {
+    if (file.exists(filename))
+    {
         sexr <- read.table(filename, header = TRUE, as.is = TRUE)
         sexr$Sex <- factor(c("Male", "Immature female", "Mature female")[sexr$sex])
     } else {
         stop(paste("can't find:", filename, "\n"))
     }
 
-  if ( PlotOptions$UsePeriod )
-  {
-    sexr$x <- sexr$period
-    xlab <- "Period"     
-  } else {
-    sexr$x <- PeriodToFishingYear(sexr$period)
-    xlab <- "Fishing year"
-  }
-
-  SD <- sqrt(sexr$Pred*(1-sexr$Pred)/sexr$effN)
-  sexr$LB <- sexr$Obs - 1.96*SD
-  sexr$UB <- sexr$Obs + 1.96*SD
-  
-  sex.lab  <- c('Male','Imm. Female','Mat. Female')
-  type.lab <- c('LB','CS')
-
-  #===============================================================================
-  # Plot obs v pred
-  #===============================================================================
-  if ( SEXROptions$ObsPred )
-  {
-    # AW
-    PlotType(paste(target.dir, "/", stock, "SEXRObsPredAW", sep = ""), PlotOptions, width = 170, height = 200)
-    par(las = 1, oma = c(1,1,0,1), mar = c(5,4,1,1), mfrow = c(3,2)) 
-    for(sex in 1:3)
-      for(type in 1:2) {
-        plot(Obs~Year,data=sexr[sexr$Season==1 & sexr$sex==sex & sexr$type==type,],pch=1,ylab="Proportion",
-          xlim=range(Year),ylim=c(min(sexr$LB),max(sexr$UB)),xlab=xlab)
-        lines(Pred~Year,data=sexr[sexr$Season==1 & sexr$sex==sex & sexr$type==type,],lwd=PlotOptions$thin)
-        #prange<-unique(sexr$Year[sexr$Season==1 & sexr$sex==sex & sexr$type==type])
-        #for(p in prange) lines(c(sexr$LB,sexr$UB)~c(p,p),data=sexr[sexr$Year==p & sexr$Season==1 & sexr$sex==sex & sexr$type==type,],pch="-",type="o",lwd=PlotOptions$thin)
-        legend('top',legend=paste(sex.lab[sex],";",type.lab[type]),lty=0,bty="n")
-      }
-    if(PlotOptions$Captions) 
+    if ( PlotOptions$UsePeriod )
     {
-      mtext(paste(source.dir," ",stock,": Observed and predicted for AW Sex Ratio fits."),side=1,line=-0.7,outer=TRUE,cex=0.7)
-      mtext(paste(" Points: Observed; Lines: Predicted  "),side=1,line=0,outer=TRUE,cex=0.7)
+        sexr$x <- sexr$period
+        xlab <- "Period"     
+    } else {
+        sexr$x <- PeriodToFishingYear(sexr$period)
+        xlab <- "Fishing year"
     }
-    dev.off()
+
+    SD <- sqrt(sexr$Pred*(1-sexr$Pred)/sexr$effN)
+    sexr$LB <- sexr$Obs - 1.96*SD
+    sexr$UB <- sexr$Obs + 1.96*SD
     
-    # SS
-    PlotType(paste(target.dir, "/", stock, "SEXRObsPredSS", sep = ""), PlotOptions, width = 170, height = 200)
-    par(las=1,oma=c(1,1,0,1),mar=c(5,4,1,1),mfrow=c(3,2)) 
-    for(sex in 1:3)
-      for(type in 1:2) {
-        plot(Obs~Year,data=sexr[sexr$Season==2 & sexr$sex==sex & sexr$type==type,],pch=1,ylab="Proportion",
-             xlim=range(Year),ylim=c(min(sexr$LB),max(sexr$UB)),xlab=xlab)
-        lines(Pred~Year,data=sexr[sexr$Season==2 & sexr$sex==sex & sexr$type==type,],lwd=PlotOptions$thin)
-        #prange<-unique(sexr$Year[sexr$Season==2 & sexr$sex==sex & sexr$type==type])
-        #for(p in prange) lines(c(sexr$LB,sexr$UB)~c(p,p),data=sexr[sexr$Year==p & sexr$Season==2 & sexr$sex==sex & sexr$type==type,],pch="-",type="o",lwd=PlotOptions$thin)
-        legend('top',legend=paste(sex.lab[sex],";",type.lab[type]),lty=0,bty="n")
-      }
-    if(PlotOptions$Captions) 
-    {
-      mtext(paste(source.dir," ",stock,": Observed and predicted for SS Sex Ratio fits."),side=1,line=-0.7,outer=TRUE,cex=0.7)
-      mtext(paste(" Points: Observed; Lines: Predicted  "),side=1,line=0,outer=TRUE,cex=0.7)
-    }
-    dev.off()
-  }
-  
+    sex.lab  <- c('Male','Imm. Female','Mat. Female')
+    type.lab <- c('LB','CS')
 
-  #===============================================================================
-  # Residuals
-  #===============================================================================
-  if ( SEXROptions$Resid )
-  {
-    # Resids v period
-    PlotType(paste(target.dir, "/", stock, "SEXRResid", sep = ""), PlotOptions, width = 170, height = 200)
-    par(las=1,oma=c(1,1,1,1),mar=c(4,3,1,1),mgp=c(2,1,0)) 
-    plot(StdRes~Year,data=sexr,pch=c(1,16)[Season],lwd=PlotOptions$thin,ylab="Standardised residual",
-         xlab=xlab)
-    abline(h=0,lty=2)
-    if(PlotOptions$Captions) 
+    #===============================================================================
+    # Plot obs v pred
+    #===============================================================================
+    if ( SEXROptions$ObsPred )
     {
-      mtext(paste(source.dir," ",stock,": Standardised residual for Sex Ratio fits"),side=1,line=-0.7,outer=TRUE,cex=0.7)
-      mtext(paste("Closed circles: SS; Open circles: AW"),side=1,line=0,outer=TRUE,cex=0.7)
-    }
-    dev.off()
-
-    # Resids v sex
-    dfm <- sexr[,c('StdRes','Sex','Year','Season')]
-    dfm$Season <- factor(c("AW","SS")[dfm$Season])
-    dfm$Sex <- factor(dfm$Sex, levels = c("Male","Immature female","Mature female"))
-    PlotType(paste(target.dir, "/", stock, "SEXRResidSex", sep = ""), PlotOptions,
-             width = 2*PlotOptions$plotsize[1], height = 10+PlotOptions$plotsize[2])
-    p <- ggplot(data = dfm, aes(x = Year, y = StdRes)) +
-             geom_hline(yintercept = 0) +
-             geom_point(size = 3) +
-             scale_colour_grey() +
-             facet_grid(Season ~ Sex) +
-             xlab(xlab) + ylab("Standardised residual") + theme_lobview(PlotOptions)
-    if ( PlotOptions$Captions )
-    {
-        p <- p + ggtitle(paste(source.dir, " ", stock, ": Standardised residual for Sex Ratio fits")) +
-                     theme(plot.title = element_text(size = 9, vjust = 2.7))
-    }
-    print(p)
-    dev.off()
-
-    #Resids v predicted
-    PlotType(paste(target.dir, "/", stock, "SEXRResidPred", sep = ""), PlotOptions, width = 170, height = 200)
-    plot(StdRes~Pred,data=sexr,pch=c(1,16)[Season],lwd=PlotOptions$thin,ylab="Standardised residual",xlab="Predicted")
-    abline(h=0,lty=2)
-    if(PlotOptions$Captions) 
-    {
-      mtext(paste(source.dir," ",stock,": Standardised residual for Sex Ratio fits"),side=1,line=-0.7,outer=TRUE,cex=0.7)
-      mtext(paste("Closed circles: SS; Open circles: AW"),side=1,line=0,outer=TRUE,cex=0.7)
-    }
-    dev.off()
+        # AW
+        PlotType(paste(target.dir, "/", stock, "SEXRObsPredAW", sep = ""), PlotOptions, width = 170, height = 200)
+        par(las = 1, oma = c(1,1,0,1), mar = c(5,4,1,1), mfrow = c(3,2)) 
+        for ( sex in 1:3 )
+        {
+            for ( type in 1:2 )
+            {
+                plot(Obs~Year,data=sexr[sexr$Season==1 & sexr$sex==sex & sexr$type==type,],pch=1,ylab="Proportion",
+                     xlim=range(Year),ylim=c(min(sexr$LB),max(sexr$UB)),xlab=xlab)
+                lines(Pred~Year,data=sexr[sexr$Season==1 & sexr$sex==sex & sexr$type==type,],lwd=PlotOptions$thin)
+                #prange<-unique(sexr$Year[sexr$Season==1 & sexr$sex==sex & sexr$type==type])
+                #for(p in prange) lines(c(sexr$LB,sexr$UB)~c(p,p),data=sexr[sexr$Year==p & sexr$Season==1 & sexr$sex==sex & sexr$type==type,],pch="-",type="o",lwd=PlotOptions$thin)
+                legend('top',legend=paste(sex.lab[sex],";",type.lab[type]),lty=0,bty="n")
+            }
+        }
+        if ( PlotOptions$Captions ) 
+        {
+            mtext(paste(source.dir," ",stock,": Observed and predicted for AW Sex Ratio fits."),side=1,line=-0.7,outer=TRUE,cex=0.7)
+            mtext(paste(" Points: Observed; Lines: Predicted  "),side=1,line=0,outer=TRUE,cex=0.7)
+        }
+        dev.off()
     
-    #qq plot
-    PlotType(paste(target.dir, "/", stock, "SEXRQQ", sep = ""), PlotOptions, width = 170, height = 200)
-    qqnorm(sexr$StdRes,pch=c(1,16)[Season],xlab="Theoretical quantiles",ylab="Standardised residual",main="",las=1)
-    qqline(sexr$StdRes)
-    abline(h=quantile(sexr$StdRes,p=c(5,25,50,75,95)/100),lty=2)#Plot quntiles of the stdevs
-    if(PlotOptions$Captions) 
-    {
-      mtext(paste(source.dir," ",stock,": Standardised residual for Sex Ratio fits"),side=1,line=-0.7,outer=TRUE,cex=0.7)
-      mtext(paste("Closed circles: SS; Open circles: AW"),side=1,line=0,outer=TRUE,cex=0.7)
+        # SS
+        PlotType(paste(target.dir, "/", stock, "SEXRObsPredSS", sep = ""), PlotOptions, width = 170, height = 200)
+        par(las=1,oma=c(1,1,0,1),mar=c(5,4,1,1),mfrow=c(3,2)) 
+        for ( sex in 1:3 )
+        {
+            for ( type in 1:2 )
+            {
+                plot(Obs~Year,data=sexr[sexr$Season==2 & sexr$sex==sex & sexr$type==type,],pch=1,ylab="Proportion",
+                     xlim=range(Year),ylim=c(min(sexr$LB),max(sexr$UB)),xlab=xlab)
+                lines(Pred~Year,data=sexr[sexr$Season==2 & sexr$sex==sex & sexr$type==type,],lwd=PlotOptions$thin)
+                #prange<-unique(sexr$Year[sexr$Season==2 & sexr$sex==sex & sexr$type==type])
+                #for(p in prange) lines(c(sexr$LB,sexr$UB)~c(p,p),data=sexr[sexr$Year==p & sexr$Season==2 & sexr$sex==sex & sexr$type==type,],pch="-",type="o",lwd=PlotOptions$thin)
+                legend('top',legend=paste(sex.lab[sex],";",type.lab[type]),lty=0,bty="n")
+            }
+        }
+        if ( PlotOptions$Captions ) 
+        {
+            mtext(paste(source.dir," ",stock,": Observed and predicted for SS Sex Ratio fits."),side=1,line=-0.7,outer=TRUE,cex=0.7)
+            mtext(paste(" Points: Observed; Lines: Predicted  "),side=1,line=0,outer=TRUE,cex=0.7)
+        }
+        dev.off()
     }
-    dev.off()
-  }
   
+
+    #===============================================================================
+    # Residuals
+    #===============================================================================
+    if ( SEXROptions$Resid )
+    {
+        # Resids v period
+        PlotType(paste(target.dir, "/", stock, "SEXRResid", sep = ""), PlotOptions, width = 170, height = 200)
+        par(las=1,oma=c(1,1,1,1),mar=c(4,3,1,1),mgp=c(2,1,0)) 
+        plot(StdRes~Year,data=sexr,pch=c(1,16)[Season],lwd=PlotOptions$thin,ylab="Standardised residual",
+             xlab=xlab)
+        abline(h=0,lty=2)
+        if(PlotOptions$Captions) 
+        {
+            mtext(paste(source.dir," ",stock,": Standardised residual for Sex Ratio fits"),side=1,line=-0.7,outer=TRUE,cex=0.7)
+            mtext(paste("Closed circles: SS; Open circles: AW"),side=1,line=0,outer=TRUE,cex=0.7)
+        }
+        dev.off()
+
+        # Resids v sex
+        dfm <- sexr[,c('StdRes','Sex','Year','Season')]
+        dfm$Season <- factor(c("AW","SS")[dfm$Season])
+        dfm$Sex <- factor(dfm$Sex, levels = c("Male","Immature female","Mature female"))
+        PlotType(paste(target.dir, "/", stock, "SEXRResidSex", sep = ""), PlotOptions,
+                 width = 2*PlotOptions$plotsize[1], height = 10+PlotOptions$plotsize[2])
+        p <- ggplot(data = dfm, aes(x = Year, y = StdRes)) +
+            geom_hline(yintercept = 0) +
+                geom_point(size = 3) +
+                    scale_colour_grey() +
+                        facet_grid(Season ~ Sex) +
+                            xlab(xlab) + ylab("Standardised residual") + theme_lobview(PlotOptions)
+        if ( PlotOptions$Captions )
+        {
+            p <- p + ggtitle(paste(source.dir, " ", stock, ": Standardised residual for Sex Ratio fits")) +
+                theme(plot.title = element_text(size = 9, vjust = 2.7))
+        }
+        print(p)
+        dev.off()
+        
+        #Resids v predicted
+        PlotType(paste(target.dir, "/", stock, "SEXRResidPred", sep = ""), PlotOptions, width = 170, height = 200)
+        plot(StdRes~Pred,data=sexr,pch=c(1,16)[Season],lwd=PlotOptions$thin,ylab="Standardised residual",xlab="Predicted")
+        abline(h=0,lty=2)
+        if(PlotOptions$Captions) 
+        {
+            mtext(paste(source.dir," ",stock,": Standardised residual for Sex Ratio fits"),side=1,line=-0.7,outer=TRUE,cex=0.7)
+            mtext(paste("Closed circles: SS; Open circles: AW"),side=1,line=0,outer=TRUE,cex=0.7)
+        }
+        dev.off()
+    
+        #qq plot
+        PlotType(paste(target.dir, "/", stock, "SEXRQQ", sep = ""), PlotOptions, width = 170, height = 200)
+        qqnorm(sexr$StdRes, pch = c(1,16)[sexr$Season], xlab = "Theoretical quantiles", ylab = "Standardised residual", main = "", las = 1)
+        qqline(sexr$StdRes)
+        abline(h=quantile(sexr$StdRes,p=c(5,25,50,75,95)/100),lty=2)#Plot quntiles of the stdevs
+        if(PlotOptions$Captions) 
+        {
+            mtext(paste(source.dir," ",stock,": Standardised residual for Sex Ratio fits"),side=1,line=-0.7,outer=TRUE,cex=0.7)
+            mtext(paste("Closed circles: SS; Open circles: AW"),side=1,line=0,outer=TRUE,cex=0.7)
+        }
+        dev.off()
+    }
 }
