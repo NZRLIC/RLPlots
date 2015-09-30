a2c03c6dcd6df8375466417b249801e140f23a2e
Charles Edwards <charles.edwards@niwa.co.nz>
2015-09-30 14:01:48 +1300

    new rebuild

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("RLPlots version 1.0.0.9002 (2015-09-25 16:20:36)")
+    packageStartupMessage("RLPlots version 1.0.0.9003 (2015-09-30 13:58:27)")
 }
