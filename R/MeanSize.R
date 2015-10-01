540b865afd950b92bde48cb491df7a227e52dc78
D'Arcy Webber <darcy@quantifish.co.nz>
2015-10-02 11:14:47 +1300

    some eds to MeanSize.R plot but not finsihed yet

--- R/MeanSize.R
+++ R/MeanSize.R
@@ -8,47 +8,46 @@
 MeanSize <- function(stock, source.dir, target.dir = source.dir,
                      LFOptions = .LFOptions, PlotOptions = .PlotOptions)
 {
-  # Read file
-  dat <- read.table(paste(source.dir, "/", stock, "FrancisLFs.out", sep = ""), header = TRUE, as.is = TRUE) 
-  # Names = sample tau period type Season sex size Obs Pred StdRes  LL 
-  maxtype <- max(dat$Type)
-  sexcode <- LFOptions$sexcode
-  
-  # label factors
-  dat$Sex[dat$Sex==1] <- "Males"
-  dat$Sex[dat$Sex==2] <- "Immature females"
-  dat$Sex[dat$Sex==3] <- "Mature females"
-  dat$Season[dat$Season==1] <- "AW"
-  dat$Season[dat$Season==2] <- "SS"
-  
-  # re-order factors
-  dat$Sex <- factor(dat$Sex, levels = c("Males","Immature females","Mature females"))
-  
-  # ==============================================================================
-  # Plot obs vs pred
-  # ==============================================================================
-  if ( LFOptions$ObsPred )
-  {
-    PlotType(paste(target.dir, "/", stock, "MeanSize" , sep = ""), PlotOptions,
-             width = 2*PlotOptions$plotsize[1], height = 10+PlotOptions$plotsize[2])
-    p <- ggplot(dat) + 
-      geom_point(aes(x = Year, y = Obs), size = 3) + 
-      geom_line(aes(x = Year, y = Pred, color = "Pred."), size = 1.1) + 
-      geom_line(aes(x = Year, y = Pred + Wt, color = "One s.d."), size = 0.5) + 
-      geom_line(aes(x = Year, y = Pred - Wt, color = "One s.d."), size = 0.5) +
-      facet_grid(Season ~ Sex) + 
-      labs(y = "Mean width (mm)", x = "Year", color = "") +
-      theme_lobview(PlotOptions) +
-      scale_colour_manual(values = c("Pred." = "black", "One s.d." = "black")) +
-      scale_size_manual(values = c("Pred." = 1.5, "One s.d." = 0.5), guide = FALSE) +
-      guides(color = guide_legend(override.aes = list(size = c(1.5,0.5)), reverse = TRUE))
-    if ( PlotOptions$Captions )
+    # Read file
+    dat <- read.table(paste(source.dir, "/", stock, "FrancisLFs.out", sep = ""), header = TRUE, as.is = TRUE) 
+    # Names = sample tau period type Season sex size Obs Pred StdRes  LL 
+    maxtype <- max(dat$Type)
+    sexcode <- LFOptions$sexcode
+    
+    # label factors
+    dat$Sex[dat$Sex==1] <- "Males"
+    dat$Sex[dat$Sex==2] <- "Immature females"
+    dat$Sex[dat$Sex==3] <- "Mature females"
+    dat$Season[dat$Season==1] <- "AW"
+    dat$Season[dat$Season==2] <- "SS"
+    
+    # re-order factors
+    dat$Sex <- factor(dat$Sex, levels = c("Males","Immature females","Mature females"))
+    
+    # ==============================================================================
+    # Plot obs vs pred
+    # ==============================================================================
+    if ( LFOptions$ObsPred )
     {
-      p <- p + ggtitle(paste(source.dir, " ", stock, ": size")) +
-        theme(plot.title = element_text(size = 9, vjust = 2.7))
+        PlotType(paste(target.dir, "/", stock, "MeanSize" , sep = ""), PlotOptions,
+                 width = 2*PlotOptions$plotsize[1], height = 10+PlotOptions$plotsize[2])
+        p <- ggplot(dat) + 
+            geom_point(aes(x = Year, y = Obs, color = Type), size = 3) + 
+            geom_line(aes(x = Year, y = Pred, color = "Pred."), size = 1.1) + 
+            geom_line(aes(x = Year, y = Pred + Wt, color = "One s.d."), size = 0.5) + 
+            geom_line(aes(x = Year, y = Pred - Wt, color = "One s.d."), size = 0.5) +
+            facet_grid(Season ~ Sex) + 
+            labs(y = "Mean width (mm)\n", x = "\nYear", color = "") +
+            theme_lobview(PlotOptions) +
+            scale_colour_manual(values = c("Pred." = "black", "One s.d." = "black")) +
+            scale_size_manual(values = c("Pred." = 1.5, "One s.d." = 0.5), guide = FALSE) +
+            guides(color = guide_legend(override.aes = list(size = c(1.5,0.5)), reverse = TRUE))
+        if ( PlotOptions$Captions )
+        {
+            p <- p + ggtitle(paste(source.dir, " ", stock, ": size")) +
+                theme(plot.title = element_text(size = 9, vjust = 2.7))
+        }
+        print(p)
+        dev.off()
     }
-    print(p)
-    dev.off()
-  }
 }
-  
