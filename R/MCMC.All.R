b4db78b57e867edf585cd0a148d5838fa60507a2
Charles Edwards <charles.edwards@niwa.co.nz>
2015-10-07 12:30:54 +1300

    fixed MCMC.All

--- R/MCMC.All.R
+++ R/MCMC.All.R
@@ -5,12 +5,18 @@
 #' @author D'Arcy Webber, Charles Edwards
 #' @export
 #' 
-MCMC.All <- function(stock, source.dir, target.dir = source.dir,
+MCMC.All <- function(stock, source.dir = ".", target.dir = paste('../', stock, 'plots', sep = ''),
                      BioOptions = .BioOptions, CROptions = .CROptions,
                      PlotOptions = .PlotOptions, MCMCOptions = .MCMCOptions)
 {
     emsg <- "OOPS - EITHER YOU FUCKED UP OR THE CODE DID, the original error message is:"
     efun <- function(e) { message(emsg); message(e) }
+    
+    if (!dir.exists(target.dir))
+    {
+        dir.create(target.dir)
+        message('created target.dir ', target.dir)
+    }
 
     message("Plotting traces")
     message("Plotting histograms")
@@ -22,30 +28,34 @@ MCMC.All <- function(stock, source.dir, target.dir = source.dir,
     tryCatch(
         RunningAvgShort(stock, source.dir, target.dir, MCMCOptions, PlotOptions),
         error = efun, finally = NULL)
-    message("Plotting CPUE posterior and fit")
-    tryCatch(
-        CPUE_posterior(stock, source.dir, target.dir, CPUEOptions, MCMCOptions, PlotOptions),
-        error = efun, finally = NULL)
-    message("Plotting catch rate posterior and fit")
-    tryCatch(
-        CR_posterior(stock, source.dir, target.dir, MCMCOptions, PlotOptions),
-        error = efun, finally = NULL)
-    message("Plotting biomass posteriors")
-    tryCatch(
-        Bio_posterior(stock, source.dir, target.dir, MCMCOptions, BioOptions, PlotOptions),
-        error = efun, finally = NULL)
-    message("Plotting recruitment posteriors")
-    tryCatch(
-        Rec_posterior(stock, source.dir, target.dir, MCMCOptions, PlotOptions),
-        error = efun, finally = NULL)
-    message("Plotting surplus production posteriors")
-    tryCatch(
-        SP_posterior(stock, source.dir, target.dir, MCMCOptions, PlotOptions),
-        error = efun, finally = NULL)
-    message("Plotting sex-ratio posteriors")
-    tryCatch(
-        SR_posterior(stock, source.dir, target.dir, PlotOptions),
-        error = efun, finally = NULL)
+    
+    for (i in 1:length(stock)) {
+        message("Plotting CPUE posterior and fit")
+        tryCatch(
+            CPUE_posterior(stock[i], source.dir, target.dir, CPUEOptions, MCMCOptions, PlotOptions),
+            error = efun, finally = NULL)
+        message("Plotting catch rate posterior and fit")
+        tryCatch(
+            CR_posterior(stock[i], source.dir, target.dir, MCMCOptions, PlotOptions),
+            error = efun, finally = NULL)
+        message("Plotting biomass posteriors")
+        tryCatch(
+            Bio_posterior(stock[i], source.dir, target.dir, MCMCOptions, BioOptions, PlotOptions),
+            error = efun, finally = NULL)
+        message("Plotting recruitment posteriors")
+        tryCatch(
+            Rec_posterior(stock[i], source.dir, target.dir, MCMCOptions, PlotOptions),
+            error = efun, finally = NULL)
+        message("Plotting surplus production posteriors")
+        tryCatch(
+            SP_posterior(stock[i], source.dir, target.dir, MCMCOptions, PlotOptions),
+            error = efun, finally = NULL)
+        message("Plotting sex-ratio posteriors")
+        tryCatch(
+            SR_posterior(stock[i], source.dir, target.dir, PlotOptions),
+            error = efun, finally = NULL)
+    }
+    
     message("Plotting maturity posteriors")
     tryCatch(
         Mature_posterior(stock, source.dir, target.dir, PlotOptions),
