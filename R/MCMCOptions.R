c4cf59e9b7c23c0744292c85fc1251ba635846b8
Charles Edwards <charles.edwards@niwa.co.nz>
2015-10-07 12:22:58 +1300

    updated help for MCMCOptions.R

--- R/MCMCOptions.R
+++ R/MCMCOptions.R
@@ -1,12 +1,16 @@
 #' MCMC options
 #'
-#' @author Darcy Webber, Charlers Edwards
-#' @param n.post number of plots in each window for histograms, trace plots and
-#' running average/quantile plots
-#' @param begin.newplot the column number where you want new plot
-#' @param Rewrite use 1 to re-calculate running mean for diagnostic otherwise 0
-#' @param running.mean the n for calculated running means in trace and running
-#' average/quantile plots
+#' List of options to be passed to MCMC plotting functions. This list is loaded with \pkg{RLPlots} and
+#' is called by each plotting function. It contains a list of values that can be modified by the user 
+#' and passed to the plotting function. Elements in the list include the following:
+#' \describe{
+#'   \item{\code{n.post =12}}{number of plots in each window for histograms, trace plots and
+#' running average/quantile plots}
+#'   \item{\code{begin.newplot = c(1,39)}}{the column number where you want new plot}
+#'   \item{\code{Rewrite = 1}}{use 1 to re-calculate running mean for diagnostic otherwise 0}
+#'   \item{\code{running.mean = 50}}{the n for calculated running means in trace and running
+#' average/quantile plots}
+#' }
 #' @export
 #' 
 .MCMCOptions <- list(n.post = 12, begin.newplot = c(1,39), Rewrite = 1,
