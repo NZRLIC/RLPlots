bbcfcbb356e8929997fa94c2fa97cad7da94168b
D'Arcy Webber <darcy@quantifish.co.nz>
2015-10-13 13:32:29 +1300

    ...

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,5 @@
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("RLPlots version 1.0.0.9023 (2015-10-12 19:45:41)")
+    packageStartupMessage("RLPlots version 1.0.0.9024 (2015-10-13 13:32:04)")
+    packageStartupMessage("To change the default plot options (e.g. plot resolution) then update the .PlotOptions list.")
 }
