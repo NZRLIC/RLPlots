bfbda0eeab93322182d95ba225ca66d15175928b
Charles Edwards <charles.edwards@niwa.co.nz>
2015-10-14 14:38:18 +1300

    added rule1 and rule2 to TACC.R

--- R/TACC.R
+++ R/TACC.R
@@ -5,18 +5,27 @@
 TACC <- function(par, cpue)
 {
     tacc <- rep(NA, length(cpue))
-    if (par[1] == 3) # Plateau rule with slope
+    # Constant TACC rule
+    if (par[1] == 1)
     {
-        #tacc <- ifelse(cpue < par[3], par[2]*(cpue - par[5])/(par[3] - par[5]) , ifelse(cpue < par[4], par[2], par[2]*(1+0.5*(cpue - par[4]) / (par[6] - par[4]))))
+        tacc[] <- par[2]
+    }
+    # constant CPUE multiplier
+    if (par[1] == 2)
+    {
+        tacc[] <- par[2] * cpue
+    }
+    # Plateau rule with slope
+    if (par[1] == 3) 
+    {
         tacc[cpue <= par[2]] <- 0
         tacc[cpue > par[2] & cpue <= par[3]] <- par[5] * ((cpue[cpue > par[2] & cpue <= par[3]] - par[2]) / (par[3] - par[2]))
         tacc[cpue > par[3] & cpue <= par[4]] <- par[5]
         tacc[cpue > par[4]] <- (cpue[cpue > par[4]] - par[4]) * 0.5 * par[5] / (par[6] - par[4]) + par[5]
     }
-    if (par[1] == 4) # Plateau rule with step function
+    # Plateau rule with step function
+    if (par[1] == 4) 
     {
-            #if (par[4] >= par[8])
-            #    stop("par4 < par8 condition not met.")
         tacc[cpue <= par[2]] <- 0
         tacc[cpue > par[2] & cpue <= par[3]] <- par[5] * ((cpue[cpue > par[2] & cpue <= par[3]] - par[2]) / (par[3] - par[2]))
         tacc[cpue > par[3] & cpue <= par[4]] <- par[5]
