c6bab1c7faa4655880dd77b7786e462b719615c3
D'Arcy Webber <darcy@quantifish.co.nz>
2015-10-14 14:24:31 +1300

    added recruitment robustness plot and compiled

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("RLPlots version 1.0.0.9046 (2015-10-13 17:15:36)")
+    packageStartupMessage("RLPlots version 1.0.0.9047 (2015-10-14 14:24:04)")
     packageStartupMessage("To change the default plot options (e.g. plot resolution) then update the .PlotOptions list.")
 }
