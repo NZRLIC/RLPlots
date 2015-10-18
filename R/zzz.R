e9cfe18dbfd812d717fc115e0dd124db1edb4125
Charles Edwards <charles.edwards@niwa.co.nz>
2015-10-19 09:08:06 +1300

    package rebuild

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("RLPlots version 1.0.0.9048 (2015-10-14 14:38:54)")
+    packageStartupMessage("RLPlots version 1.0.0.9049 (2015-10-14 15:54:21)")
     packageStartupMessage("To change the default plot options (e.g. plot resolution) then update the .PlotOptions list.")
 }
