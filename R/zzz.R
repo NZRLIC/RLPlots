cadac5b16683301ea4f3a4937e93e0ceb4512a4b
quantifish <darcy@quantifish.co.nz>
2016-09-06 13:51:25 +1200

    rebuild

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("RLPlots version 1.0.0.9054 (2016-06-10 09:46:42)")
+    packageStartupMessage("RLPlots version 1.0.0.9056 (2016-09-06 13:41:43)")
     packageStartupMessage("To change the default plot options (e.g. plot resolution) then update the PlotOptions list.")
 }
